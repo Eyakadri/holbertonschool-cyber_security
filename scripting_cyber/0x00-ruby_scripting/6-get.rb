a1c71385eb2feca13005e7b0db6d4e3a04fbeb1d
Eyakadri <eyaakadrii@gmail.com>
2025-05-23 11:14:44 +0100

    6-get.rb

--- scripting_cyber/0x00-ruby_scripting/6-get.rb
+++ scripting_cyber/0x00-ruby_scripting/6-get.rb
@@ -6,13 +6,25 @@ def get_request(url)
   begin
     content = URI.open(url)
     status_line = content.status.join(' ')
+    body = content.read
+
     puts "Response status: #{status_line}"
     puts "Response body:"
-    json = JSON.parse(content.read)
-    puts JSON.pretty_generate(json)
+
+    if body.strip.empty?
+      puts "{}"
+    else
+      begin
+        json = JSON.parse(body)
+        puts JSON.pretty_generate(json)
+      rescue JSON::ParserError
+        puts "{}"
+      end
+    end
   rescue OpenURI::HTTPError => e
-    puts "HTTP Request failed: #{e.message}"
-  rescue JSON::ParserError
-    puts "Response is not valid JSON."
+    status_line = e.io.status.join(' ')
+    puts "Response status: #{status_line}"
+    puts "Response body:"
+    puts "{}"
   end
 end
