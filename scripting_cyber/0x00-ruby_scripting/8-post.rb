fa3615438aa4332f6d5a434f22b817d43456860f
Eyakadri <eyaakadrii@gmail.com>
2025-05-23 11:38:02 +0100

    8-post.rb

--- scripting_cyber/0x00-ruby_scripting/8-post.rb
+++ scripting_cyber/0x00-ruby_scripting/8-post.rb
@@ -1,18 +1,19 @@
 #!/usr/bin/env ruby
 require 'net/http'
-require 'json'
 require 'uri'
+require 'json'
 
-def post_request(url, body_params)
-  uri = URI(url)
-  request = Net::HTTP::Post.new(uri, 'Content-Type' => 'application/json')
-  request.body = body_params.to_json
+def post_request(url, body_params = {})
+  uri = URI.parse(url)
+
+  request = Net::HTTP::Post.new(uri)
+  request.content_type = 'application/json'
+  request.body = body_params.to_json unless body_params.empty?
 
   response = Net::HTTP.start(uri.hostname, uri.port, use_ssl: uri.scheme == 'https') do |http|
     http.request(request)
   end
-
   puts "Response status: #{response.code} #{response.message}"
   puts "Response body:"
-  puts response.body.empty? ? '{}' : JSON.pretty_generate(JSON.parse(response.body))
+  puts JSON.pretty_generate(JSON.parse(response.body))
 end
